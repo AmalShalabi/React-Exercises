2c409f70adddaed16c7be342781cb55fc3df3b05
unknown <mouni.shalaby@gmail.com>
2022-06-29 22:19:35 +0300

    useContext-hook

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+import React from "react";
+import ComponentC from "./components/useContext/ComponentC";
 import DataFetching from "./components/useEffetct/DataFetching";
 import IndividualPost from "./components/useEffetct/IndividualPost";
 import RandomUser from "./components/useEffetct/RandomUser";
 
+export const UserContext = React.createContext();
+export const ChannelContext = React.createContext();
+
 function App() {
- 
 
   return (
-    <div>
-    {/* <RandomUser/> */}
-    {/* <DataFetching/> */}
-    <IndividualPost/>
+    <div className="container">
+      <UserContext.Provider value={"Amal"}>
+        <ChannelContext.Provider value={"Shalabi"}>
+          <ComponentC />
+        </ChannelContext.Provider>
+      </UserContext.Provider>
+      {/* <RandomUser/> */}
+      {/* <DataFetching/> */}
+      {/* <IndividualPost/> */}
     </div>
   );
 }
